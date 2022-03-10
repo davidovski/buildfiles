bb499959d88da1b3937c23b5405089c907188a81
davidovski <david@davidovski.xyz>
2022-03-10 11:59:16 +0000

    added and patched broken packages

--- auto/create_meta.sh
+++ auto/create_meta.sh
@@ -3,12 +3,12 @@
 create() {
     local desc=$*
 
-    printf "#!/bin/bash\n"
+    printf "#!/bin/sh\n"
     printf "# This file was automatically generated, do not edit!"
     printf "\n\n"
 
     printf "DESC=\"$desc\"\n" 
-    printf "DEPS=("
+    printf "DEPS=\""
     while read repo; do
         [ -d repo/$repo ] && [ ! "$repo" = "meta" ] &&
             for file in $(ls repo/$repo/*.xibuild); do
@@ -17,18 +17,19 @@ create() {
             done
     done
 
-    printf ")\n"
+    printf "\"\n"
 }
 
 ls repo | create 'AlL tHe pacKageS!!' > repo/meta/all.xibuild
 
-skip="skip meta linux"
+skip="skip meta"
 
 for repo in $(ls repo); do
+    pkg_name=repo-$repo
     if echo $skip | grep -q $repo; then
         echo "Skipping $repo"
     else
-        echo $repo | create "All the the packages available in $repo" > repo/meta/$repo.xibuild
-        echo "Generated $repo.xibuild"
+        echo $repo | create "All the the packages available in $repo" > repo/meta/$pkg_name.xibuild
+        echo "Generated $pkg_name.xibuild"
     fi
 done
